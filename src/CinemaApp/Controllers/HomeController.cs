f38af916aeb15741502578c1d117ca8081f7de27
YavorStoychev <qwor1006@gmail.com>
2026-04-07 12:31:13 +0300

    Scaffold of default identity. Clean up and implementation of custom logic. Generate migration.

--- src/CinemaApp/Controllers/HomeController.cs
+++ src/CinemaApp/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ namespace CinemaApp.Web.Controllers
     using Microsoft.AspNetCore.Mvc;
     public class HomeController : Controller
     {
-
         public HomeController()
         {
 
--- src/CinemaApp/Program.cs
+++ src/CinemaApp/Program.cs
@@ -7,12 +7,13 @@ namespace CinemaApp.Web
     {
         public static void Main(string[] args)
         {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("SqlServerDev") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services
